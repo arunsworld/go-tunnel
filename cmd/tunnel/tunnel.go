0bdaff05b07c3dbdb77239935586da6045ddf74d
Arun Barua <arun.barua@e2open.com>
2023-06-15 12:23:50 +0100

    added a secrets section; user prompt for secret if env not provided

--- cmd/tunnel/tunnel.go
+++ cmd/tunnel/tunnel.go
@@ -5,12 +5,70 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"syscall"
 
 	tunnel "github.com/arunsworld/go-tunnel"
 	"github.com/arunsworld/nursery"
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/term"
 	"gopkg.in/yaml.v2"
 )
+
+type tunnelConfig struct {
+	Secrets    []secret
+	SshConfigs []sshConfig `json:"sshconfigs"`
+}
+
+type secret struct {
+	Name string
+	Env  string
+}
+
+type secretsVault map[string]vaultSecret
+
+func (s secretsVault) secretFor(k string) (vaultSecret, error) {
+	v, ok := s[k]
+	if !ok {
+		return "", fmt.Errorf("secret %s not setup", k)
+	}
+	return v, nil
+}
+
+type vaultSecret string
+
+func newSecretsVault(rawSecrets []secret) (secretsVault, error) {
+	result := make(secretsVault)
+	for _, s := range rawSecrets {
+		sv, err := newSecretVault(s)
+		if err != nil {
+			return nil, err
+		}
+		result[s.Name] = sv
+	}
+	return result, nil
+}
+
+func newSecretVault(s secret) (vaultSecret, error) {
+	if s.Name == "" {
+		return "", fmt.Errorf("secret specified without a name")
+	}
+	if s.Env != "" {
+		v := os.Getenv(s.Env)
+		if v != "" {
+			return vaultSecret(v), nil
+		}
+	}
+	fmt.Printf("Enter value for secret %s: ", s.Name)
+	bytePassword, err := term.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		return "", fmt.Errorf("error reading secret from prompt for %s: %v", s.Name, err)
+	}
+	if bytePassword == nil || string(bytePassword) == "" {
+		return "", fmt.Errorf("error - no value provided for secret %s", s.Name)
+	}
+	fmt.Println("")
+	return vaultSecret(string(bytePassword)), nil
+}
 
 type sshConfig struct {
 	Destination string
@@ -18,6 +76,16 @@ type sshConfig struct {
 	Auth        []auth
 	Tunnels     []portForward
 	ThroughSSH  []sshConfig
+}
+
+func (sc *sshConfig) validateAndUpdateAuth(vault secretsVault) error {
+	for i, a := range sc.Auth {
+		if err := a.validateAndUpdate(vault); err != nil {
+			return err
+		}
+		sc.Auth[i] = a
+	}
+	return nil
 }
 
 type portForward struct {
@@ -32,23 +100,66 @@ type auth struct {
 	PwdAuth pwdAuth
 }
 
+func (a *auth) validateAndUpdate(vault secretsVault) error {
+	if err := a.KeyAuth.validateAndUpdate(vault); err != nil {
+		return err
+	}
+	if err := a.PwdAuth.validateAndUpdate(vault); err != nil {
+		return err
+	}
+	return nil
+}
+
 type keyAuth struct {
-	FileLocation string
-	PasswordEnv  string
+	FileLocation   string
+	PasswordSecret string
+	// internal
+	password vaultSecret
+}
+
+func (a *keyAuth) validateAndUpdate(vault secretsVault) error {
+	if a.PasswordSecret != "" {
+		v, err := vault.secretFor(a.PasswordSecret)
+		if err != nil {
+			return err
+		}
+		a.password = v
+	}
+	return nil
 }
 
 type pwdAuth struct {
-	PasswordEnv string
+	PasswordSecret string
+	// internal
+	password vaultSecret
 }
 
-func (sc *sshConfig) validate() error {
+func (a *pwdAuth) validateAndUpdate(vault secretsVault) error {
+	if a.PasswordSecret != "" {
+		v, err := vault.secretFor(a.PasswordSecret)
+		if err != nil {
+			return err
+		}
+		a.password = v
+	}
+	return nil
+}
+
+func (sc *sshConfig) validateAndUpdate(vault secretsVault) error {
 	if sc.Destination == "" {
 		return fmt.Errorf("config has empty destination")
 	}
-	for _, pf := range sc.ThroughSSH {
+	if err := sc.validateAndUpdateAuth(vault); err != nil {
+		return err
+	}
+	for i, pf := range sc.ThroughSSH {
 		if pf.Destination == "" {
 			return fmt.Errorf("ThroughSSH config has empty destination")
 		}
+		if err := pf.validateAndUpdateAuth(vault); err != nil {
+			return err
+		}
+		sc.ThroughSSH[i] = pf
 	}
 	return nil
 }
@@ -71,13 +182,17 @@ func run(ctx context.Context, conf *config) error {
 	if err != nil {
 		return fmt.Errorf("unable to open config file %s: %v", conf.configFile, err)
 	}
-	sshConf := []sshConfig{}
-	if err := yaml.Unmarshal(contents, &sshConf); err != nil {
+	tunnelConf := tunnelConfig{}
+	if err := yaml.Unmarshal(contents, &tunnelConf); err != nil {
 		return fmt.Errorf("unable to parse config file %s: %v", conf.configFile, err)
 	}
+	vault, err := newSecretsVault(tunnelConf.Secrets)
+	if err != nil {
+		return err
+	}
 	jobs := []nursery.ConcurrentJob{}
-	for i, c := range sshConf {
-		if err := c.validate(); err != nil {
+	for i, c := range tunnelConf.SshConfigs {
+		if err := c.validateAndUpdate(vault); err != nil {
 			return fmt.Errorf("invalid config #%d: %v", i, err)
 		}
 		jobs = append(jobs, jobForConfig(ctx, c))
@@ -100,23 +215,16 @@ func sshAuthFromAuth(auth auth) (ssh.AuthMethod, error) {
 	switch {
 	case auth.KeyAuth.FileLocation != "":
 		pwd := ""
-		if auth.KeyAuth.PasswordEnv != "" {
-			pwd = os.Getenv(auth.KeyAuth.PasswordEnv)
-			if pwd == "" {
-				return nil, fmt.Errorf("env variable %s not set", auth.KeyAuth.PasswordEnv)
-			}
+		if auth.KeyAuth.PasswordSecret != "" {
+			pwd = string(auth.KeyAuth.password)
 		}
 		key, err := tunnel.PrivateKeyFile(auth.KeyAuth.FileLocation, pwd)
 		if err != nil {
 			return nil, fmt.Errorf("unable to create private key auth: %v", err)
 		}
 		return key, nil
-	case auth.PwdAuth.PasswordEnv != "":
-		pwd := os.Getenv(auth.PwdAuth.PasswordEnv)
-		if pwd == "" {
-			return nil, fmt.Errorf("env variable %s not set", auth.PwdAuth.PasswordEnv)
-		}
-		return ssh.Password(pwd), nil
+	case auth.PwdAuth.PasswordSecret != "":
+		return ssh.Password(string(auth.PwdAuth.password)), nil
 	default:
 		return nil, fmt.Errorf("invalid auth details")
 	}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ require (
 	github.com/gliderlabs/ssh v0.3.3
 	github.com/urfave/cli/v2 v2.25.3
 	golang.org/x/crypto v0.9.0
+	golang.org/x/term v0.8.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
